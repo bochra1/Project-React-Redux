2ab40da34d0703ae5139f2c2340798fbac278827
hatem <hatem.diouani@sastec-group.com>
2022-07-13 15:53:17 +0100

    second-commit

---
+++ src/store/reducers/AuthReducer.js
@@ -0,0 +1,10 @@
+const initState = {
+    posts:[]
+}
+
+const AuthReducer = (state= initState, action) => {
+    
+    return ( state );
+}
+ 
+export default AuthReducer;
---
+++ src/store/reducers/ProjectReducer.js
@@ -0,0 +1,6 @@
+const initState= {}
+const ProjectReducer = (state, action) => {
+    return (  state);
+}
+ 
+export default ProjectReducer;
